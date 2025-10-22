20a33148d63823e5e3e47311c9162f75d65b6d8e
po_hsi <is33.wu@gmail.com>
2025-10-22 10:47:28 +0800

    Disable public registration feature.

--- web_app/app.py
+++ web_app/app.py
@@ -991,21 +991,8 @@ def login():
 @app.route("/register", methods=['GET', 'POST'])
 @log_request
 def register():
-    # This route is now open for public registration
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            flash('此使用者名稱已被註冊', 'error')
-            return redirect(url_for('register'))
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        new_user = User(username=username, password_hash=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('會員帳號註冊成功!請登入', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html')
+    flash('註冊功能已關閉，請聯絡管理員以取得帳號。', 'info')
+    return redirect(url_for('login'))
 
 @app.route("/logout")
 @login_required
